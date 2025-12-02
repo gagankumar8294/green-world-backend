e1ed4ba3a9dc05a46d18608c1918ef4b7ebeba4d
gagankumar8294 <gagankumar8294@gmail.com>
2025-12-02 21:42:23 +0530

    minor modifictions

--- src/products/productRoutes.js
+++ src/products/productRoutes.js
@@ -1,12 +1,12 @@
 // routes/productRoutes.js
 import express from "express";
 import { addProduct } from "./productController.js";
-import { getProductsInfinite } from "./productController.js";
+import { getProducts } from "./productController.js";
 
 const ProductRouter = express.Router();
 
 ProductRouter.post('/add', addProduct);
 // 🔥 Infinite loading + filters + search + sorting API
-ProductRouter.get('/list', getProductsInfinite);
+ProductRouter.get('/list', getProducts);
 
 export default ProductRouter;
